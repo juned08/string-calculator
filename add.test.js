cf3eb23d27a7ff6429838c66c282b1548cc1c402
Juned Shaikh <junaidshaikh888@gmail.com>
2025-07-19 16:27:19 +0530

    change: case for multiple numbers

--- add.test.js
+++ add.test.js
@@ -3,6 +3,10 @@ import { add } from './add.js';
 try {
   console.assert(add("") === 0, `Failed: Input :"" should return 0`);
   console.assert(add('1') === 1, `Failed: Input :"1" should return 1`);
+  console.assert(add('1,2') === 3, `Failed: Input :"1,2" should return 3`);
+  console.assert(add('1,2,3') === 6, `Failed: Input :"1,2,3" should return 6`);
+
+
   
 } catch (error) {
   console.error(" Error: ", error.message);
