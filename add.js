ccef49a521f9b81a695f69b242d511c52b164d10
Juned Shaikh <junaidshaikh888@gmail.com>
2025-07-19 16:20:47 +0530

    Change: case for single number input

--- add.js
+++ add.js
@@ -1,3 +1,5 @@
 export function add(numbers) {
   if(numbers === "") return 0;
+
+  return numbers.split(",").map(Number).reduce((a ,b) => a + b);
 }
--- add.test.js
+++ add.test.js
@@ -2,6 +2,8 @@ import { add } from './add.js';
 
 try {
   console.assert(add("") === 0, `Failed: Input :"" should return 0`);
+  console.assert(add('1') === 1, `Failed: Input :"1" should return 1`);
+  
 } catch (error) {
   console.error(" Error: ", error.message);
 }
